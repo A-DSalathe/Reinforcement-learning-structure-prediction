950064e552ef67d3a1ccce0f851a51fb23d112ae
Arnaud Wist <arnaud.wist@gmail.com>
2024-05-23 19:01:49 +0200

    added plot 3d of other molecule

--- save_plot.py
+++ save_plot.py
@@ -6,12 +6,12 @@ from mpl_toolkits.mplot3d import Axes3D
 import torch
 script_dir = op.dirname(op.realpath(__file__))
 
-def save_array(array,title):
+def save_array(array, title):
     folder_name = 'numpy_array_folder'
-    folder_path = op.join(script_dir,folder_name)
+    folder_path = op.join(script_dir, folder_name)
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    file_path = op.join(folder_path,title+'.npy')
+    file_path = op.join(folder_path, title + '.npy')
     np.save(file_path, array)
 
 def plot_scores(scores, title, display=False):
@@ -23,17 +23,16 @@ def plot_scores(scores, title, display=False):
 
     plt.figure(figsize=(10, 5))
     plt.plot(filtered_scores, label='Score per Episode')
-    plt.ylim(
-        [np.min(filtered_scores) - 1, np.max(filtered_scores) + 1])  # Adjust y-axis limits for better visualization
+    plt.ylim([np.min(filtered_scores) - 1, np.max(filtered_scores) + 1])  # Adjust y-axis limits for better visualization
     plt.xlabel('Episode')
     plt.ylabel('Score')
     plt.title('Evolution of the Score over Episodes')
     plt.legend()
     folder_score = 'score'
-    folder_path = op.join(script_dir,folder_score)
+    folder_path = op.join(script_dir, folder_score)
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    file_path = op.join(folder_path,title+'.png')
+    file_path = op.join(folder_path, title + '.png')
     plt.savefig(file_path)
     if display:
         plt.show()
@@ -46,13 +45,26 @@ def plot_sphere(ax, center, radius, color='r'):
     z = center[2] + radius * np.outer(np.ones(np.size(u)), np.cos(v))
     ax.plot_surface(x, y, z, color=color, alpha=0.6)
 
+def calculate_centroid(positions):
+    return np.mean(positions, axis=0)
+
+def center_molecule(positions, grid_dimensions, resolution):
+    centroid = calculate_centroid(positions)
+    grid_center = np.array(grid_dimensions) * resolution / 2.0
+    shift_vector = grid_center - centroid
+    shifted_positions = positions + shift_vector
+    return shifted_positions
 
 def plot_and_save_view(positions, resolution, grid_dimensions, view_angle, title, display=False):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    for pos in positions:
-        plot_sphere(ax, pos, radius=0.1)
+    # Center the molecule
+    centered_positions = center_molecule(np.array(positions), grid_dimensions, resolution)
+
+    # Plot spheres at atom positions
+    for pos in centered_positions:
+        plot_sphere(ax, pos, radius=0.1, color='r')  # Adjust the radius and color as needed
 
     # Set labels
     ax.set_xlabel('X')
@@ -81,7 +93,6 @@ def plot_and_save_view(positions, resolution, grid_dimensions, view_angle, title
     else:
         plt.close(fig)  # Close the figure to free up memory
 
-
 def plot_spectra(arrays, title, display=False):
     plt.figure(figsize=(10, 5))
     ref_frequency = arrays[0]
@@ -94,20 +105,20 @@ def plot_spectra(arrays, title, display=False):
     plt.ylabel('Intensity')
     plt.legend()
     folder_spectra = 'spectra'
-    folder_path = op.join(script_dir,folder_spectra)
+    folder_path = op.join(script_dir, folder_spectra)
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    file_path = op.join(folder_path,title+'.png')
+    file_path = op.join(folder_path, title + '.png')
     plt.savefig(file_path)
     if display:
         plt.show()
-        
-def save_weights(policy,name):
+
+def save_weights(policy, name):
     folder_weight = 'weight'
-    folder_path = op.join(script_dir,folder_weight)
+    folder_path = op.join(script_dir, folder_weight)
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    file_path = op.join(folder_path,name+'.pth')
+    file_path = op.join(folder_path, name + '.pth')
     torch.save(policy.state_dict(), file_path)
 
 def plot_eval_loss_and_rewards(eval_losses, eval_rewards, title, display=False):
@@ -137,8 +148,35 @@ def plot_eval_loss_and_rewards(eval_losses, eval_rewards, title, display=False):
     plt.savefig(file_path)
     if display:
         plt.show()
+
+def read_npy(path):
+    data = np.genfromtxt(path, dtype=float, skip_header=2, usecols=(1, 2, 3))
+    return data
     
 if __name__ == "__main__":
+    positions = [[-0.57500000, -0.57500000, 0.00000000],
+                 [-0.57500000, 0.57500000, -1.15000000],
+                 [-1.72500000, 0.57500000, 0.00000000],
+                 [-0.57500000, 0.57500000, 1.15000000],
+                 [-0.57500000, 1.72500000, 0.00000000],
+                 [0.57500000, -0.57500000, -1.15000000],
+                 [0.57500000, -1.72500000, 0.00000000],
+                 [0.57500000, -0.57500000, 1.15000000],
+                 [1.72500000, -0.57500000, 0.00000000],
+                 [0.57500000, 0.57500000, -2.30000000],
+                 [0.57500000, 1.72500000, -1.15000000],
+                 [1.72500000, 0.57500000, -1.15000000],
+                 [0.57500000, 0.57500000, 0.00000000],
+                 [0.57500000, 1.72500000, 1.15000000],
+                 [1.72500000, 0.57500000, 1.15000000],
+                 [0.57500000, 0.57500000, 2.30000000]]
+    positions = read_npy(op.join(script_dir,op.join('references','reference_5_B.xyz')))
+    resolution = np.array([0.2, 0.2, 0.2])
+    grid_dimensions = (21, 21, 21)
+    number_of_atoms = 16
+    plot_and_save_view(positions, resolution, grid_dimensions, view_angle=[30, 30],
+                       title='3d_structure_' + str(number_of_atoms) + '_atoms', display=True)
+    
     test_array = np.array([[1,2,4],[1,2,3]])
     test_name = 'name'
     save_array(array=test_array,title=test_name)
